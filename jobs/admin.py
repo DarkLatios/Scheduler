968f99b5cf3d019e87e7a8d9b40f5d544f8e0efa
Aditya Srivastava <aditya.srivastava@tatadigital.com>
2021-01-10 12:17:10 +0530

    Initial Commit

---
+++ jobs/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Hall,Condition
+
+admin.site.register(Hall)
+admin.site.register(Condition)
---
+++ jobs/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Hall(models.Model):      #For Every Hall I have its Condition/Status.
+    name=models.CharField(max_length=200,primary_key = True,blank=False)
+    maxcapacity=models.IntegerField()
+    vacancy=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Condition(models.Model):
+    hall=models.ForeignKey(Hall,on_delete=models.CASCADE,related_name="condition")
+    day=models.DateField(auto_now_add=False)
+    time = models.TimeField(auto_now_add=False)
+    status=models.CharField(max_length=200,default="available")
+
+
+    def __str__(self):
+        return self.status
