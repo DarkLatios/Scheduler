fe7804dfbf7502476571dd498bef18ba1eb3e892
Aditya Srivastava <aditya.srivastava@tatadigital.com>
2021-01-12 00:01:23 +0530

    Wrapper Api

--- jobs/views.py
+++ jobs/views.py
@@ -4,12 +4,22 @@ from rest_framework import serializers,status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Hall,Condition,Img
+import requests
 
 class HallList(APIView):
     def get(self,request):
         hall=Hall.objects.all()
         serializer=HallSerializer(hall,many=True)
         return Response(serializer.data)
+    def post(self,request):
+
+        url="https://swapi.dev/api/people/1/"
+        response=requests.get(url)
+        data=response.json()
+        print(data['vehicles'])
+        
+
+        return Response(None)
 
 class Images(APIView):
     def get(self,request):
